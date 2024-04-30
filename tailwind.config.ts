ccedbc7028ffb303e46ed77c133b34ed1ac5d645
Isbat-Bin-Hossain <isbat31416@gmail.com>
2024-04-30 20:24:39 +0600

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#2b9e76',
+        accent: '#ebfcf6',
+        'hadith-bg': '#f2f4f6',
+        'hadith-bg-lite': '#fafbfb',
       },
     },
+    screens: {
+      sm: '350px',
+      md: '1024px',
+      lg: '1280px',
+      xl: '1536px',
+    },
+    fontFamily: {
+      kalpurush: ['Kalpurush', ...defaultTheme.fontFamily.sans],
+      KFGQPC: ['KFGQOC Uthmanic Script', ...defaultTheme.fontFamily.sans],
+    },
   },
+
   plugins: [],
-};
-export default config;
+}
+export default config
